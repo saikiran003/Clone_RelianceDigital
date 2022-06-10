e26a30a439d6f93bde0a06537e98448e6fedac1a
saikiran003 <kiranarishanpalli@gmail.com>
2022-06-10 16:58:06 +0530

    navbar completed

--- reliance/src/App.js
+++ reliance/src/App.js
@@ -2,12 +2,14 @@ import Header from "./Components/Header";
 import "./Styles.css"
 import "@fortawesome/react-fontawesome"
 import MainHeader from "./Components/MainHeader";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div >
       <Header/>
       <MainHeader/>
+      <Navbar/>
     </div>
   );
 }
---
+++ reliance/src/Components/Navbar.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <div>
+          MOBILES & TABLETS
+          <span
+            class="fa fa-angle-down ml__4"
+            style={{ marginLeft: "5px" }}
+          ></span>
+        </div>
+
+        <div>
+          TV & AUDIO
+          <span
+            class="fa fa-angle-down ml__4"
+            style={{ marginLeft: "5px" }}
+          ></span>
+        </div>
+        <div>
+          HOME APPLIANCES
+          <span
+            class="fa fa-angle-down ml__4"
+            style={{ marginLeft: "5px" }}
+          ></span>
+        </div>
+        <div>
+          COMPUTERS
+          <span
+            class="fa fa-angle-down ml__4"
+            style={{ marginLeft: "5px" }}
+          ></span>
+        </div>
+        <div>
+          CAMERAS
+          <span
+            class="fa fa-angle-down ml__4"
+            style={{ marginLeft: "5px" }}
+          ></span>
+        </div>
+        <div>
+          KITCHEN APPLIANCES
+          <span
+            class="fa fa-angle-down ml__4"
+            style={{ marginLeft: "5px" }}
+          ></span>
+        </div>
+        <div>
+          PERSONAL CARE
+          <span
+            class="fa fa-angle-down ml__4"
+            style={{ marginLeft: "5px" }}
+          ></span>
+        </div>
+        <div>
+          ACCESSORIES
+          <span
+            class="fa fa-angle-down ml__4"
+            style={{ marginLeft: "5px" }}
+          ></span>
+        </div>
+      </div>
+
+      <div className="dropDown">
+
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
