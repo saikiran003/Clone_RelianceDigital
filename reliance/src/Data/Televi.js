eee7650aa868f25c298329b5de1c3bc7f4bf5aed
saikiran003 <kiranarishanpalli@gmail.com>
2022-06-15 23:22:59 +0530

    filtering left

--- reliance/src/Data/Televi.js
+++ reliance/src/Data/Televi.js
@@ -3,7 +3,7 @@ export const Telvi = [
         id:1,
         name:"IFFALCON 81.28 cm (32 inch) HD Android Smart TV with Dolby Surround Sound Technology, 32F52",
         img:"https://www.reliancedigital.in/medias/IFFALCON-32F52-Televisions-492403800-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w0OTU5OXxpbWFnZS9qcGVnfGltYWdlcy9oODcvaDc0Lzk3MjcxMzgxMzYwOTQuanBnfDM1N2Q4MTYxMTcyZWMwZjIyM2EzMWJkNTA0OGE5YTQ4OGM2MTlkMDFhYzQyYzRkM2E3MDFlYzQ3MWJhYWQ0YjM",
-        price:"12,990.00",
+        price:12990.00,
         mrp:"26,990.00",
         discount:"52%(₹14,000)",
         brand:"IFFALCON"
@@ -12,7 +12,7 @@ export const Telvi = [
         id:2,
         name:"Karbonn 80 cm (32 inch) HD Ready Smart LED TV, Millenium Series KJW32SKHD",
         img:"https://www.reliancedigital.in/medias/Karbonn-KJW32SKHD-LED-TV-492391971-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wxNjMwNXxpbWFnZS9qcGVnfGltYWdlcy9oYjkvaDZkLzk4NDE5MTU1OTI3MzQuanBnfGFmYjJlNTkyODE0Y2UxOTk4NWEwNjY2Y2E0NzVhMWNiMjZmZmYyOGE3M2YwZGFiOTQyNDViYmQxNTcyNWM0NDc",
-        price:"10,999.00",
+        price:10999.00,
         mrp:"18,990.00",
         discount:"42%(₹7,991)",
         brand:"Karbonn"
@@ -21,7 +21,7 @@ export const Telvi = [
         id:3,
         name:"Kodak 80 cm (32 inch) HD Ready LED Smart TV, 7XPRO Series 32HDX7XPRO, Quad Core Processor, Built-in Chromecast",
         img:"https://www.reliancedigital.in/medias/Kodak-32HDX7XPRO-Television-492166141-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w4MzcxOHxpbWFnZS9qcGVnfGltYWdlcy9oNGUvaGI4Lzk1MTE0NzEzNDk3OTAuanBnfDE3MTQ4OGE5OWIzNWUwNjRkZDE0OTRkNTM3MDhjZTAwOGVjYTc4OWZjNzQ0MTdhNzczYzQ5ZTgyYWE2ODRlYzI",
-        price:"12,499.00",
+        price:12499.00,
         mrp:"18,499.00",
         discount:"32%(₹6,000)",
         brand:"Kodak"
@@ -30,7 +30,7 @@ export const Telvi = [
         id:4,
         name:"Karbonn 80 cm (32 inch) HD Ready LED TV, Millenium Series KJW32NSHD",
         img:"https://www.reliancedigital.in/medias/Karbonn-KJW32NSHD-LED-TVS-492403971-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wxNzI0MnxpbWFnZS9qcGVnfGltYWdlcy9oMDcvaDc3Lzk4NDIxMDgxMzc1MDIuanBnfDdkM2Q1ZmIxYTVmMTcyYTA2YjIzODY3MDhkMzI3OGE5NTdhOWZiNDk4MmYxYmUyMDZkY2M5MTRiNTllMTc1Y2E",
-        price:"9,999.00",
+        price:9999.00,
         mrp:"16,990.00",
         discount:"41%(₹6,991)",
         brand:"Karbonn"
@@ -39,7 +39,7 @@ export const Telvi = [
         id:5,
         name:"Karbonn 60 cm (24 inch) HD Ready LED TV, Millenium Series KJW24NSHD",
         img:"https://www.reliancedigital.in/medias/Karbonn-KJW24NSHD-LED-TV-492403972-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wxMzc5NXxpbWFnZS9qcGVnfGltYWdlcy9oMGEvaDRlLzk4Mzg5NTY0MTI5NTguanBnfGZjYTUwNWZlN2U1NGQxZmI0YTljMWQ4ZjExMTY4YjlkOThlZDBiZDAxMGMyNWU0YjlmNmZjODliZGQ2YTk4YmY",
-        price:"7,499.00",
+        price:7499.00,
         mrp:"11,990.00",
         discount:"37%(₹4,491)",
         brand:"Karbonn"
@@ -48,7 +48,7 @@ export const Telvi = [
         id:6,
         name:"Toshiba 126 cm (50 inch) 4K Ultra HD Vidaa OS Smart LED TV with Dolby Vision and ATMOS, 50U5050",
         img:"https://www.reliancedigital.in/medias/Toshiba-50U5050-Televisions-491893301-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w3NDc0MXxpbWFnZS9wbmd8aW1hZ2VzL2hlNS9oZTAvOTgyMDI0OTI5MjgzMC5wbmd8MjI1OGE3ODAyNjc2OGMxMzNkYmNlYWEzOGVjOGQwZGZjNDM0ZmNiZWQ2ZTRjZWExODBhMmUyMjgyNDZkZmNiMg",
-        price:"33,990.00",
+        price:33990.00,
         mrp:"46,990.00",
         discount:"28%(₹13,000)",
         brand:"Toshiba"
@@ -57,7 +57,7 @@ export const Telvi = [
         id:7,
         name:"Kodak 102 cm (40 inch) Full HD LED Smart TV, 7XPRO Series 40FHDX7XPRO, Built-in Chromecast, Quad Core Processor",
         img:"https://www.reliancedigital.in/medias/Kodak-40FHDX7XPRO-Television-492166142-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w3ODkxNXxpbWFnZS9qcGVnfGltYWdlcy9oMWYvaGFkLzk1MTE0NzI5ODgxOTAuanBnfDQwY2U3NmQwZTQ4YmFkZWZlZWExMDFkYWQwMjhlODc4OTVhN2IxYTdkNmViNWM3NzFkMDVjYTdhNzNjOTEzZTg",
-        price:"17,990.00",
+        price:17990.00,
         mrp:"24,999.00",
         discount:"28%(₹7,009)",
         brand:"Kodak"
@@ -66,7 +66,7 @@ export const Telvi = [
         id:8,
         name:"LG 81.28 cm (32 inch) HD LED Smart TV, 32LM562BPTA (2021 Model)",
         img:"https://www.reliancedigital.in/medias/LG-32LM562BPTA-Televisions-492338698-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w5NDEyMnxpbWFnZS9qcGVnfGltYWdlcy9oZDEvaDY3Lzk2MDU1NDcwMzI2MDYuanBnfDM1MWQzMTEzYmEyYWY2MGI0OTViYjJjZmNmNGMxZTUzZTViNTJiOTQ0MmY2ODg1ZDgwOWZhYjdmZmNiNGNkZmU",
-        price:"17,990.00",
+        price:17990.00,
         mrp:"20,990.00",
         discount:"14%(₹3,000)",
         brand:"LG"
@@ -75,7 +75,7 @@ export const Telvi = [
         id:9,
         name:"ONEPLUS 81.28 cm (32 inch) HD Smart  LED TV, 32Y1",
         img:"https://www.reliancedigital.in/medias/ONEPLUS-32Y1-TV-491895010-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyMjIzMXxpbWFnZS9qcGVnfGltYWdlcy9oNjkvaDE0Lzk4MTAwNTg5MDM1ODIuanBnfGFiZjk3NDA1MzY1ODQwYjc2OTZkMzY4OTI1Y2RmOWM3OTI4NTE1MTlkMDFlOTUwYmZkM2U5MTVlYmIyZGFlYzg",
-        price:"15,499.00",
+        price:15499.00,
         mrp:"19,999.00",
         discount:"23%(₹4,500)",
         brand:"ONEPLUS"
@@ -84,7 +84,7 @@ export const Telvi = [
         id:10,
         name:"Sansui 109 cm (43 inch) Full HD LED Smart TV, Prime Series JSW43ASFHD",
         img:"https://www.reliancedigital.in/medias/Sansui-JSW43ASFHD-Televisions-491973451-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w4NTIzOXxpbWFnZS9qcGVnfGltYWdlcy9oY2EvaDI2Lzk0NjcwMjM0NTgzMzQuanBnfGMzZDIyYjdiYTIwMDUxYzdkNTRiZDQyYmJiYmY3Yzg2MDA1ZjhmMGI2NDY2MWQ4NWU2NDhmNzQyZDIzYTY1YWU",
-        price:"22,999.00",
+        price:22999.00,
         mrp:"30,990.00",
         discount:"26%(₹7,991)",
         brand:"Sansui"
@@ -93,7 +93,7 @@ export const Telvi = [
         id:11,
         name:"Hisense 108 cm (43 inch) 4K Ultra HD Android Smart LED TV With Dolby Vision and ATMOS, 43A71F",
         img:"https://www.reliancedigital.in/medias/Hisense-43A71F-LED-Televisions-491893292-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w0ODU3NnxpbWFnZS9qcGVnfGltYWdlcy9oZmYvaGQ4Lzk3ODkyNDA0NDI5MTAuanBnfDNlNjc4MzI3N2FhOTljYmMyOWJmMWJkMDIwMTc3OTY4NTMyYWYzNjMzNjFhY2EyNmE2MjA3OGM2NWE3MWI4MGY",
-        price:"29,990.00",
+        price:29990.00,
         mrp:"36,990.00",
         discount:"19%(₹7,000)",
         brand:"Hisense"
@@ -102,7 +102,7 @@ export const Telvi = [
         id:12,
         name:"LG 108 cm (43 inch) Full HD LED Smart TV, 43LM5620 (2021 Model)",
         img:"https://www.reliancedigital.in/medias/LG-43-FHD-Smart-LED-TV-43LM5620-492338697-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w4MDIzN3xpbWFnZS9qcGVnfGltYWdlcy9oODIvaGU2Lzk2MDU4ODEyMzM0MzguanBnfDMwYzA2NDRmNjc1ZTBkN2QyNDdmN2Q1NDA2MGRiYWE3NmY4YmFlNDk3NTdkMzI1N2YxODQyODczMWRkYWY2NDg",
-        price:"33,990.00",
+        price:33990.00,
         mrp:"40,990.00",
         discount:"17%(₹7,000)",
         brand:"LG"
@@ -111,16 +111,16 @@ export const Telvi = [
         id:13,
         name:"HISENSE 108 cm (43 inch) Full HD Android Smart LED TV, A56F 43A56F",
         img:"https://www.reliancedigital.in/medias/HISENSE-43A56F-Televisions-492166307-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w3Mzk1N3xpbWFnZS9qcGVnfGltYWdlcy9oNzYvaGZlLzk2NDYwNjk2NzgxMTAuanBnfDcxYzJjMmMzMjljZjExMWI1MDAxYzIyMjlmOWRmYjEwMzYxNWE1MTRhMDVkZmJhM2I5OWUwMTgyZTNjNzJhNzU",
-        price:"24,990.00",
+        price:24990.00,
         mrp:"39,990.00",
         discount:"38%(₹15,000)",
-        brand:"HISENSE"
+        brand:"Hisense"
     },
     {
         id:14,
         name:"Kodak 108 cm (43 inch) Ultra HD (4K) Premium Android TV with Dolby Digital Plus and DTS TruSurround, CA Series 43CA2022",
         img:"https://www.reliancedigital.in/medias/Kodak-43CA2022-Television-492166175-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w2ODUwMnxpbWFnZS9qcGVnfGltYWdlcy9oOGIvaDEwLzk1MTIxMDYyMjk3OTAuanBnfDgwNTMwNDExYzI5MjkyNDJkZmZlMTI0ODAwYmM4OGNlZTZiM2U3MWQzM2FhNWI1ZDJmOGE5ZDJhYzg3ZjkxZGM",
-        price:"22,990.00",
+        price:22990.00,
         mrp:"37,999.00",
         discount:"39%(₹15,009)",
         brand:"Kodak"
@@ -129,7 +129,7 @@ export const Telvi = [
         id:15,
         name:"BPL 60.96 cm (24 inch) HD Ready LED TV, A1000 24H-A1000",
         img:"https://www.reliancedigital.in/medias/BPL-24H-A1000-Television-491895043-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyNDM0NnxpbWFnZS9qcGVnfGltYWdlcy9oZDcvaDBmLzk3ODIzMDM5NDg4MzAuanBnfDFhODMwYWE3OThmMzNjYjZjMDUyNGU0ZmYxNTA3ZDRkMGM4ZWMxN2RlY2U5MWQ2MWVhZTNlYTM3ZGRkNTZiNGE",
-        price:"11,499.00",
+        price:11499.00,
         mrp:"15,000.00",
         discount:"23%(₹3,501)",
         brand:"BPL"
@@ -138,7 +138,7 @@ export const Telvi = [
         id:16,
         name:"MI 40 FHD SMART LED TV, 4A HORIZON, ELA4642IN",
         img:"https://www.reliancedigital.in/medias/Mi-ELA4642IN-Televisions-492391655-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w4MzMwMnxpbWFnZS9qcGVnfGltYWdlcy9oOGEvaDIyLzk2MjUwNTA0MTUxMzQuanBnfDcwMmY3Mjc0ODVjOTRhMzUzZjFhMjBmODQxZDAzYzVmYTA0ODI3YTc4OTU4Mjc4MTQ4YzJkMTU4NTgxOTNkZjQ",
-        price:"21,999.00",
+        price:21999.00,
         mrp:"29,999.00",
         discount:"27%(₹8,000)",
         brand:"MI"
@@ -147,7 +147,7 @@ export const Telvi = [
         id:17,
         name:"Kodak 164 cm (65 inch) Ultra HD (4K) Premium Android TV with Dolby Digital Plus and DTS TruSurround, CA Series 65CA0101",
         img:"https://www.reliancedigital.in/medias/Kodak-65CA0101-Television-492166178-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w2OTE4NHxpbWFnZS9qcGVnfGltYWdlcy9oMjEvaDYwLzk1MTIxMDY1NTc0NzAuanBnfDk3MWQ2Nzc4YTZjNjIwYzIyMTg2YTA3ZWEzMjc0N2Y3MWZlYTI4NzAzY2JhY2RmZGEyODA0ZGNhNGZlY2EyODk",
-        price:"54,999.00",
+        price:54999.00,
         mrp:"70,499.00",
         discount:"22%(₹15,500)",
         brand:"Kodak"
@@ -156,7 +156,7 @@ export const Telvi = [
         id:18,
         name:"Toshiba 108 cm (43 inch) Full HD Vidaa OS Smart LED TV with ADS Panel and dbx-tv, 43L5050",
         img:"https://www.reliancedigital.in/medias/Toshiba-43L5050-Televisions-491893299-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w0MTMzNHxpbWFnZS9qcGVnfGltYWdlcy9oZjUvaGY4LzkzNzQ0MjU0ODEyNDYuanBnfDNlNzcxZDIzNjI3MTBkMzYzNzRmNGRkNWNlNjljZTg1YjlkZmQ4OTYyOWYyODhjZDAxNTljNGUwYmE0NWE1M2M",
-        price:"25,990.00",
+        price:25990.00,
         mrp:"31,990.00",
         discount:"19%(₹6,000)",
         brand:"Toshiba"
@@ -165,7 +165,7 @@ export const Telvi = [
         id:19,
         name:"Redmi 80 cm (32 inch) HD Smart LED ELA4915/4732/01IN",
         img:"https://www.reliancedigital.in/medias/Xiaomi-ELA4701IN-Televisions-492571689-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w4NzQyMnxpbWFnZS9qcGVnfGltYWdlcy9oZjUvaDZmLzk2OTE2NjA0NTE4NzAuanBnfDA0NzYyMTg0OTQ4ZWZhYTMyM2Y1MTdiNmFhYmQ4MDczYjM1OTA0OTY4MDYwYWZmZjYzMTljMmZhYTFkZjMyYmY",
-        price:"13,999.00",
+        price:13999.00,
         mrp:"24,999.00",
         discount:"44%(₹11,000)",
         brand:"Redmi"
@@ -174,7 +174,7 @@ export const Telvi = [
         id:20,
         name:"Sansui 60 cm (24 inch) HD Ready LED TV, Prime Series JSY24NSHD",
         img:"https://www.reliancedigital.in/medias/Sansui-JSY24NSHD-Prime-Series-LED-TV-491973455-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w5NzQ2MHxpbWFnZS9qcGVnfGltYWdlcy9oNmQvaGUyLzk2MTA0NTEwNTg3MTguanBnfDdlNzVkNWJlZjMyYzhkMGZiMDJkYmY1NWM4ZWIxZDM4M2I1ODI3YWQxZjg4ZGFmOTM2ZTUzYWZlMzZiYmE2NTU",
-        price:"8,999.00",
+        price:8999.00,
         mrp:"11,490.00",
         discount:"22%(₹2,491)",
         brand:"Sansui"
@@ -183,7 +183,7 @@ export const Telvi = [
         id:21,
         name:"IFFALCON 109.22 cm (43 inch) Full HD LED Smart TV, 43F2A (Android 8.0, Dolby Decoder Technology for Enhance Audio-Quality)",
         img:"https://www.reliancedigital.in/medias/IFFALCON-43F2A-Television-492338468-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w1MTMwNXxpbWFnZS9qcGVnfGltYWdlcy9oOWIvaDg5Lzk1ODA4NzU2Nzc3MjYuanBnfDk3ZmIwN2RjZTUxNTA1YTBmMzk5MWY4YjA3OGJmYjJkNGQ3MDFjNmJhNjU1Yjk5MWRhZTNmNTI3ZTE0ZmExNTc",
-        price:"21,990.00",
+        price:21990.00,
         mrp:"45,990.00",
         discount:"52%(₹24,000)",
         brand:"IFFALCON"
@@ -192,7 +192,7 @@ export const Telvi = [
         id:22,
         name:"Kodak 108 cm (43 inch) Ultra HD (4K) LED Smart TV, 7XPRO Series 43UHDX7XPRO, Powerful 24 Watts Speaker, Multiple Connectivity Option",
         img:"https://www.reliancedigital.in/medias/Kodak-43UHDX7XPRO-Television-492166143-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w3MzM0NHxpbWFnZS9qcGVnfGltYWdlcy9oMWIvaGY4Lzk1MTE0NzE2Nzc0NzAuanBnfGU5OWFmNDFlYTcwYTllZTU4ZTYzMTMxNjgzOWZkMjFiNDI2MzY5ZDU2ZDhhOTY3ZWIyNGU3ZTQzMGEwMzMxYmQ",
-        price:"22,999.00",
+        price:22999.00,
         mrp:"33,999.00",
         discount:"32%(₹11,000)",
         brand:"Kodak "
@@ -201,7 +201,7 @@ export const Telvi = [
         id:23,
         name:"Kodak 80 cm (32 inch) HD Ready LED TV, 32HDX900s",
         img:"https://www.reliancedigital.in/medias/Kodak-32HDX900S-Televisions-491431274-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w4MjQ1MHxpbWFnZS9qcGVnfGltYWdlcy9oYjMvaGIxLzkzMjQ0NzM5NzQ4MTQuanBnfGE2YmQwOWViYzM4ZDQ2ZTczMjE3MWNkZTU4ZjY0YWU4YmUzMjQ2NzM5MGM3Y2NjZjBjNzk4MjdmZjEzNTJlZGE",
-        price:"9,999.00",
+        price:9999.00,
         mrp:"16,990.00",
         discount:"41%(₹6,991)",
         brand:"Kodak"
@@ -210,7 +210,7 @@ export const Telvi = [
         id:24,
         name:"Sony Bravia 80 cm (32 inches) HD Ready Smart Android LED TV 32W830 (Black) (2021 Model)",
         img:"https://www.reliancedigital.in/medias/SONY-32W830-Smart-LED-TV-492166305-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wxNDQ0NXxpbWFnZS9qcGVnfGltYWdlcy9oYWMvaDE2Lzk2MTQyMTMyMTgzMzQuanBnfGE3ZjE5YmRkNDQ1MTNlMzEyZTQ3YmQzZTY4ZDVkNzc2M2UxNzZjZmRjYTJkNWFkYzVjNjYzOTA3ODk2Mjg1Yjk",
-        price:"27,990.00",
+        price:27990.00,
         mrp:"34,900.00",
         discount:"20%(₹6,910)",
         brand:"Sony"
--- reliance/src/Pages/Televisions.jsx
+++ reliance/src/Pages/Televisions.jsx
@@ -1,8 +1,45 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { Carousel } from "react-bootstrap";
 import { Telvi } from "../Data/Televi";
 
 const Televisions = () => {
+  const [product, setProduct] = useState(Telvi);
+
+  const handleSort = (e) => {
+    const value = e.target.value;
+    if (value === "l2h") {
+      const updatedProducts = [...product].sort((a, b) => a.price - b.price);
+      setProduct(updatedProducts);
+    }
+    if (value == "h2l") {
+      const updatedProducts = [...product].sort((a, b) => b.price - a.price);
+      setProduct(updatedProducts);
+    }
+    if (value === "a2z") {
+      const updatedProducts = [...product].sort((a, b) =>
+        a.name.localeCompare(b.name)
+      );
+      setProduct(updatedProducts);
+    }
+    if (value === "z2a") {
+      const updatedProducts = [...product].sort((a, b) =>
+        b.name.localeCompare(a.name)
+      );
+      setProduct(updatedProducts);
+    }
+  };
+
+  const handleFilter = (e)=>{
+    const value = e.target.value;
+    if(value === "Karbonn")
+    {
+      const updatedProducts = [...product].filter((x)=>x.brand==="Karbonn")
+      setProduct(updatedProducts)
+    }
+
+  }
   return (
     <div>
       <div>
@@ -45,11 +82,107 @@ const Televisions = () => {
         </Carousel>
       </div>
       <div className="home">
-        <i class="fa fa-home" aria-hidden="true" style={{fontSize:"10px",marginTop:"5px"}}></i>
-        <p style={{fontSize:"14px",color:"#000000B3"}}>Televisions</p>
+        <i
+          class="fa fa-home"
+          aria-hidden="true"
+          style={{ fontSize: "10px", marginTop: "5px" }}
+        ></i>
+        <p style={{ fontSize: "14px", color: "#000000B3" }}>Televisions</p>
       </div>
       <div className="tv">
-        <div></div>
+        <div>
+          <h3 class="filters">FILTERS</h3>
+          <div>
+            <h4 className="rs">Price</h4>
+            <div className="slider">
+              <div className="progress"></div>
+            </div>
+            <div className="range-input">
+              <input
+                type="range"
+                className="rang-min"
+                min="0"
+                max="55000"
+                value="6500"
+              />
+              <input
+                type="range"
+                className="rang-max"
+                min="0"
+                max="55000"
+                value="55000"
+              />
+            </div>
+            <div className="slider_input">
+              <p>Min</p>
+              <input type="number" value="6500" />
+              <p>to</p>
+              <p>Max</p>
+              <input type="number" value="55000" />
+            </div>
+          </div>
+          <div className="brands">
+            <h4 className="rs">Brand</h4>
+            <form >
+              <div className="checkbox">
+                <input type="checkbox" value="Karbonn" onChange={handleFilter}/>
+                <span className="brand-name">Karbonn</span>
+              </div>
+              <div className="checkbox">
+                <input type="checkbox" value="Kodak" />
+                <span className="brand-name">Kodak</span>
+              </div>
+              <div className="checkbox">
+                <input type="checkbox" value="Toshiba" />
+                <span className="brand-name">Toshiba</span>
+              </div>
+              <div className="checkbox">
+                <input type="checkbox" value="LG" />
+                <span className="brand-name">LG</span>
+              </div>
+              <div className="checkbox">
+                <input type="checkbox" value="ONEPLUS" />
+                <span className="brand-name">One Plus</span>
+              </div>
+              <div className="checkbox">
+                <input type="checkbox" value="Sansui" />
+                <span className="brand-name">Sansui</span>
+              </div>
+              <div className="checkbox">
+                <input type="checkbox" value="Hisense" />
+                <span className="brand-name">Hisense</span>
+              </div>
+              <div className="checkbox">
+                <input type="checkbox" value="Sony" />
+                <span className="brand-name">Sony</span>
+              </div>
+              <div className="checkbox">
+                <input type="checkbox" value="IFFALCON" />
+                <span className="brand-name">Iffalcon</span>
+              </div>
+            </form>
+          </div>
+          <div className="brands">
+            <h4 className="rs">Discount Percent</h4>
+            <div className="checkbox">
+              <input type="checkbox" value="60%" />
+              <span className="brand-name">60% or more</span>
+            </div>
+            <div className="checkbox">
+              <input type="checkbox" value="40%" />
+              <span className="brand-name">40% to 60%</span>
+            </div>
+            <div className="checkbox">
+              <input type="checkbox" value="20%" />
+              <span className="brand-name">20% to 40%</span>
+            </div>
+            <div className="checkbox">
+              <input type="checkbox" value="10%" />
+              <span className="brand-name">10% to 20%</span>
+            </div>
+          </div>
+        </div>
+
         <div>
           <div
             style={{
@@ -62,12 +195,15 @@ const Televisions = () => {
               <h1 style={{ fontSize: "24px" }}>TELEVISIONS</h1>
             </div>
             <div style={{ display: "flex" }}>
-              <select style={{width:"200px",height:"30px",fontSize:"14px"}}>
-                <option>Sort By</option>
-                <option>Name(A-Z)</option>
-                <option>Name(Z-A)</option>
-                <option>Price(Low-High)</option>
-                <option>Price(High-Low)</option>
+              <select
+                style={{ width: "200px", height: "30px", fontSize: "14px" }}
+                onChange={handleSort}
+              >
+                <option value="">Sort By</option>
+                <option value="a2z">Name(A-Z)</option>
+                <option value="z2a">Name(Z-A)</option>
+                <option value="l2h">Price(Low-High)</option>
+                <option value="h2l">Price(High-Low)</option>
               </select>
               <div
                 style={{
@@ -77,7 +213,10 @@ const Televisions = () => {
                   marginLeft: "18px",
                 }}
               >
-                <ul className="list-inline" style={{display:"flex",gap:"10px"}}>
+                <ul
+                  className="list-inline"
+                  style={{ display: "flex", gap: "10px" }}
+                >
                   <li class="">
                     <span>
                       <div className="fa fa-bars"></div>
@@ -93,9 +232,12 @@ const Televisions = () => {
             </div>
           </div>
           <div className="tvGrid">
-            {Telvi.map((item) => {
+            {product.map((item) => {
               return (
-                <div style={{ border: "1px solid #e0e0e1",height:"328px" }}>
+                <div
+                  style={{ border: "1px solid #e0e0e1", height: "328px" }}
+                  key={item.id}
+                >
                   <div
                     style={{
                       width: "180px",
@@ -112,7 +254,13 @@ const Televisions = () => {
                   </div>
                   <div>
                     <p>{item.name}</p>
-                    <div style={{ display: "flex", padding: "0px 16px",marginTop:"8px" }}>
+                    <div
+                      style={{
+                        display: "flex",
+                        padding: "0px 16px",
+                        marginTop: "8px",
+                      }}
+                    >
                       <span
                         style={{
                           color: "#1F4985",
@@ -120,7 +268,7 @@ const Televisions = () => {
                           fontWeight: "600",
                         }}
                       >
-                        ₹{item.price}
+                        ₹{item.price.toLocaleString("en-US")}
                       </span>
                       <span
                         style={{
@@ -136,23 +284,56 @@ const Televisions = () => {
                           color: "rgb(67, 160, 71)",
                           fontSize: "12px",
                           fontWeight: "600",
-                          marginLeft:"8px"
+                          marginLeft: "8px",
                         }}
                       >
                         {item.discount}
                       </span>
                     </div>
-                    
                   </div>
                   <div className="available">OFFERS AVAILABLE</div>
-                  <div style={{border:"1px solid #e0e0e1",height:"48px",display:"flex"}}>
-                    <div style={{width:"50%",margin:"auto",padding:"5px 25px",borderRight:"1px solid #e0e0e1 "}}>
-                        <input type="checkbox" style={{marginRight:"8px",backgroundColor:"rgb(37,73,133)",border:"1px solid rgb(37,73,133)"}}/>
-                        <span style={{color:"#1F4985",fontSize:"14px"}}>Compare</span>
+                  <div
+                    style={{
+                      border: "1px solid #e0e0e1",
+                      height: "48px",
+                      display: "flex",
+                    }}
+                  >
+                    <div
+                      style={{
+                        width: "50%",
+                        margin: "auto",
+                        padding: "5px 25px",
+                        borderRight: "1px solid #e0e0e1 ",
+                      }}
+                    >
+                      <input
+                        type="checkbox"
+                        style={{
+                          marginRight: "8px",
+                          backgroundColor: "rgb(37,73,133)",
+                          border: "1px solid rgb(37,73,133)",
+                        }}
+                      />
+                      <span style={{ color: "#1F4985", fontSize: "14px" }}>
+                        Compare
+                      </span>
                     </div>
-                    <div style={{width:"50%",margin:"auto",padding:"5px 25px"}}>
-                    <i class="fa fa-heart-o" aria-hidden="true" style={{marginRight:"8px"}}></i>
-                    <span style={{color:"#1F4985",fontSize:"14px"}}>Whishlist</span>
+                    <div
+                      style={{
+                        width: "50%",
+                        margin: "auto",
+                        padding: "5px 25px",
+                      }}
+                    >
+                      <i
+                        class="fa fa-heart-o"
+                        aria-hidden="true"
+                        style={{ marginRight: "8px" }}
+                      ></i>
+                      <span style={{ color: "#1F4985", fontSize: "14px" }}>
+                        Whishlist
+                      </span>
                     </div>
                   </div>
                 </div>
